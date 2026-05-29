257d6652f38d3e12b1e9c2d66c7ba99341b7249b
nrahman <nrahman@dept-info.crosemont.quebec>
2026-05-29 08:50:34 -0400

    Addition and substraction works

--- calculator.js
+++ calculator.js
@@ -1,12 +1,28 @@
 const buttons = document.querySelectorAll("button");
+const equalBtn = document.querySelector("#equalBtn");
+const acBtn = document.querySelector("#acBtn");
+
 const outputResult = document.querySelector("#result");
 /* ---- TEST ----
 buttons.forEach(button => {
     console.log(button.textContent);
 });
 */
-buttons.forEach((button) => {
-  button.addEventListener("click", function(event) {
-    outputResult.value += event.target.textContent;
+buttons.forEach(button => {
+  button.addEventListener("click", function() {
+    if (button.id === "equalBtn" || button.id === "acBtn") return;
+    outputResult.value += button.textContent
+
+    if (button.id === "multiplyBtn") {
+      outputResult.value *= button.textContent
+    }
   });
-});
+});  
+
+equalBtn.addEventListener("click", function() {
+    outputResult.value = eval(outputResult.value);
+})
+
+acBtn.addEventListener("click", function() {
+    outputResult.value = "";
+})
